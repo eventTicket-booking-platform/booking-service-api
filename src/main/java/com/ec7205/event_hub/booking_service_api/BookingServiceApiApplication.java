2a03e854acdd13f2572f5a599754f6c626ba5a64
AmiruHoradagoda <amirumithsara1234@gmail.com>
2026-04-04 20:32:07 +0530

    first commit

---
+++ src/main/java/com/ec7205/event_hub/booking_service_api/BookingServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ec7205.event_hub.booking_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ec7205/event_hub/booking_service_api/BookingServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec7205.event_hub.booking_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
